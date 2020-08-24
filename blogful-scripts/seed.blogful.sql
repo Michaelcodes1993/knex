5da72294211c09f7658106bfc06435d62f857983
michaelcodes1993 <lpskirito7@outlook.com>
2020-08-24 09:42:04 -0400

    fist commit

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,25 @@
+BEGIN; 
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+    ('Blog1', 'A Political Blog','10/02/2020'),
+    ('Blog2', 'A Food Blog', '12/11/2008'),
+    ('Blog3', 'An Independent Blog', '13/12/2010'),
+    ('Blog4', 'A Fashion Blog', '13/03/2019'),
+    ('Blog5', 'A Sports Blog', '03/12/2018'),
+    ('Blog6', 'A Fashion Blog', '15/03/2017'),
+    ('Blog7', 'A Interior Design Blog', '13/06/2016'),
+    ('Blog8', 'A Political Blog', '13/07/2016'),
+    ('Blog9', 'A University Blog', '13/08/2015'),
+    ('Blog10', 'A Book Blog', '13/09/2016'),
+    ('Blog11', 'A Tourist Blog', '23/10/2011'),
+    ('Blog12', 'A School Blog', '21/01/2010'),
+    ('Blog13', 'Dining Blog', '13/03/2009'),
+    ('Blog14', 'A Cooking Blog', '23/08/2008'),
+    ('Blog15', 'A Science Blog', '13/05/2007'),
+    ('Blog16', 'A Political Science Blog', '23/06/2006'),
+    ('Blog17', 'A History Blog', '03/07/2005'),
+    ('Blog18', 'A Mom Blog', '03/08/2004'),
+    ('Blog19', 'A Russian Blog', '13/09/2003'),
+    ('Blog20', 'A Math Blog', '03/02/2002')
+; 
+COMMIT; 
